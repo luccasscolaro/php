12a829771694ea290b6e1758cac808586da19266
luccasscolaro <luccasscolaro16@gmail.com>
2023-10-30 13:54:58 -0300

    sistema novo

---
+++ sastemaAdiministrativo/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="recursos/estilo.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+<body class="funo">
+
+    <div class="painel">
+        <i class="fa-solid fa-user perfil"></i>
+        <h1 class="login">login:</h1>
+        <form>
+            <div class="cpf">
+                <label for="exampleInputEmail1">CPF</label>
+                <input name="cpf" type="text" class="cpf" placeholder="Seu CPF" required>
+            </div>
+
+            <div class="senha">
+                <label for="exampleInputPassword1">Senha</label>
+                <input name="senha" type="password" class="senha" placeholder="Senha" required>
+            </div>
+
+            <a href="principal.php">principal</a>
+
+            <button type="submit" class="btn btn-primary bunto1">Enviar</button>
+            <button type="reste" class="btn btn-primary bunto2">Limpar</button>
+        </form>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ sastemaAdiministrativo/menu.php
@@ -0,0 +1,8 @@
+<ul>
+    <li> <a class="item" href="pricincipal.php"> pricincipal</a> </li>
+    <li> <a class="item" href="profissao.php"> profissão</a> </li>
+    <li> <a class="item" href="funcinario.php"> funcinario</a> </li>
+    <li> <a class="item" href="curso.php"> curso</a> </li>
+    <li> <a class="item" href="Agendamento.php">Agendamento </a> </li>
+    <li> <a class="sair" href="sair.php">sair </a> </li>
+</ul>
